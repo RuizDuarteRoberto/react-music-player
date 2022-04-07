d6e936bb88442c52c31a1e08734752f335cc004d
Roberto Ruiz-Duarte <robertoruizduarte2010@gmail.com>
2022-04-07 09:53:45 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { fortawesome } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faPlay,
+  faPause,
+  faStepBackward,
+  faStepForward,
+  faVolumeUp,
+  faVolumeMute,
+  faVolumeDown,
+} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Music Player</h1>
+      <div className="player">
+        <div className="player-controls">
+          <FontAwesomeIcon icon={faStepBackward} />
+          <FontAwesomeIcon icon={faPlay} />
+          <FontAwesomeIcon icon={faPause} />
+          <FontAwesomeIcon icon={faStepForward} />
+        </div>
+        <div className="player-progress">
+          <div className="player-progress-bar">
+            <div className="player-progress-bar-fill"></div>
+          </div>
+        </div>
+
+        <div className="player-volume">
+          <FontAwesomeIcon icon={faVolumeUp} />
+          <FontAwesomeIcon icon={faVolumeMute} />
+          <FontAwesomeIcon icon={faVolumeDown} />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
